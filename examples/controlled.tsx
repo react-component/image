e975af1f202bc3cc9b6b1f0e627c2f0295e2298e
shaodahong <darryshaw@gmail.com>
2020-09-24 14:51:36 +0800

    add test, example

---
+++ examples/controlled.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Image from '../src';
+import '../assets/index.less';
+
+export default function Base() {
+  const [visible, setVisible] = React.useState(false);
+  return (
+    <div>
+      <div>
+        <button
+          type="button"
+          onClick={() => {
+            setVisible(true);
+          }}
+        >
+          Switch Preview
+        </button>
+      </div>
+      <Image
+        src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+        width={200}
+        previewVisible={visible}
+        onPreviewClose={() => {
+          setVisible(false);
+        }}
+      />
+    </div>
+  );
+}
