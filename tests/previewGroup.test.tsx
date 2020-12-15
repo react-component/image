edff0c3519653cf496d82cf1e51b78156de12e14
shaodahong <darryshaw@gmail.com>
2020-12-15 17:13:22 +0800

    add previewGroup.test.tsx

---
+++ tests/previewGroup.test.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
+import Image from '../src';
+
+describe('Preview', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  it('Mount and UnMount', () => {
+    const wrapper = mount(
+      <Image.PreviewGroup>
+        <Image src="src1" />
+        <Image src="src2" />
+      </Image.PreviewGroup>,
+    );
+
+    act(() => {
+      wrapper
+        .find('.rc-image')
+        .at(0)
+        .simulate('click');
+      jest.runAllTimers();
+      wrapper.update();
+    });
+
+    expect(wrapper.find('.rc-image-preview').get(0)).toBeTruthy();
+
+    expect(() => {
+      wrapper.unmount();
+    }).not.toThrow();
+  });
+
+  it('Disable preview', () => {
+    const wrapper = mount(
+      <Image.PreviewGroup>
+        <Image src="src1" preview={false} />
+      </Image.PreviewGroup>,
+    );
+
+    act(() => {
+      wrapper
+        .find('.rc-image')
+        .at(0)
+        .simulate('click');
+      jest.runAllTimers();
+      wrapper.update();
+    });
+
+    expect(wrapper.find('.rc-image-preview').get(0)).toBeFalsy();
+  });
+
+  it('Switch', () => {
+    const wrapper = mount(
+      <Image.PreviewGroup>
+        <Image src="src1" />
+        <Image src="src2" preview={false} />
+        <Image src="src3" />
+      </Image.PreviewGroup>,
+    );
+
+    act(() => {
+      wrapper
+        .find('.rc-image')
+        .at(0)
+        .simulate('click');
+      jest.runAllTimers();
+      wrapper.update();
+    });
+
+    expect(
+      wrapper.find('.rc-image-preview .rc-image-preview-switch-left-disabled').get(0),
+    ).toBeTruthy();
+
+    act(() => {
+      wrapper.find('.rc-image-preview .rc-image-preview-switch-right').simulate('click');
+      jest.runAllTimers();
+      wrapper.update();
+    });
+
+    expect(
+      wrapper.find('.rc-image-preview .rc-image-preview-switch-right-disabled').get(0),
+    ).toBeTruthy();
+
+    act(() => {
+      wrapper.find('.rc-image-preview .rc-image-preview-switch-left').simulate('click');
+      jest.runAllTimers();
+      wrapper.update();
+    });
+
+    expect(
+      wrapper.find('.rc-image-preview .rc-image-preview-switch-left-disabled').get(0),
+    ).toBeTruthy();
+  });
+});
