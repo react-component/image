58450e5f3accb193247abcbfec1420289ef367d0
二货机器人 <smith3816@gmail.com>
2023-09-20 14:12:27 +0800

    test: update test

--- tests/fallback.test.tsx
+++ tests/fallback.test.tsx
@@ -2,17 +2,27 @@ import { act, fireEvent, render } from '@testing-library/react';
 import React from 'react';
 import Image from '../src';
 
+global.lastResolve = null;
+
 jest.mock('../src/util', () => {
   const { isImageValid, ...rest } = jest.requireActual('../src/util');
 
   return {
     ...rest,
-    isImageValid: () => Promise.resolve(false),
+    isImageValid: () =>
+      new Promise(resolve => {
+        global.lastResolve = resolve;
+
+        setTimeout(() => {
+          resolve(false);
+        }, 1000);
+      }),
   };
 });
 
 describe('Fallback', () => {
   beforeEach(() => {
+    global.lastResolve = null;
     jest.useFakeTimers();
   });
 
@@ -50,18 +60,19 @@ describe('Fallback', () => {
     expect(document.querySelector('.rc-image-preview-img')).toHaveAttribute('src', fallback);
   });
 
-  it('should not show preview', () => {
+  it('should not show preview', async () => {
     const { container } = render(<Image src="abc" fallback={fallback} />);
 
     fireEvent.error(container.querySelector('.rc-image-img'));
-    act(() => {
+    await act(async () => {
       jest.runAllTimers();
+      await Promise.resolve();
     });
 
     expect(container.querySelector('.rc-image-mask')).toBeFalsy();
   });
 
-  it('should change image, not error', () => {
+  it('should change image, not error', async () => {
     const { container, rerender } = render(
       <Image
         width={200}
@@ -70,8 +81,6 @@ describe('Fallback', () => {
       />,
     );
 
-    fireEvent.error(container.querySelector('.rc-image-img'));
-
     rerender(
       <Image
         width={200}
@@ -79,9 +88,18 @@ describe('Fallback', () => {
         fallback="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
       />,
     );
-    act(() => {
-      jest.runAllTimers();
+
+    // New Image should pass
+    await act(async () => {
+      await global.lastResolve(true);
     });
+
+    // Origin one should failed
+    await act(async () => {
+      jest.runAllTimers();
+      await Promise.resolve();
+    });
+
     expect(container.querySelector('.rc-image-img')).toHaveAttribute(
       'src',
       'https://gw.alipayobjects.com/mdn/rms_08e378/afts/img/A*NZuwQp_vcIQAAAAAAAAAAABkARQnAQ',
