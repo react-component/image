8f6af3726252c70f0744540f81110eb0a8580257
kiner-tang(文辉) <1127031143@qq.com>
2023-11-28 15:09:26 +0800

    feat: operation default icons

---
+++ docs/examples/common.tsx
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import type { PreviewProps } from '@/Preview';
+import {
+    CloseOutlined,
+    LeftOutlined,
+    RightOutlined,
+    RotateLeftOutlined,
+    RotateRightOutlined,
+    SwapOutlined,
+    ZoomInOutlined,
+    ZoomOutOutlined,
+  } from '@ant-design/icons';
+  
+  export const defaultIcons: PreviewProps['icons'] = {
+    rotateLeft: <RotateLeftOutlined />,
+    rotateRight: <RotateRightOutlined />,
+    zoomIn: <ZoomInOutlined />,
+    zoomOut: <ZoomOutOutlined />,
+    close: <CloseOutlined />,
+    left: <LeftOutlined />,
+    right: <RightOutlined />,
+    flipX: <SwapOutlined />,
+    flipY: <SwapOutlined rotate={90} />,
+  };
