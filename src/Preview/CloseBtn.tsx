82898fd9c30119decf04e07252ac4f1c1ba087a1
arktomson <2372929539@qq.com>
2025-09-25 22:56:51 +0800

    feat: adapt semantic close

--- src/Preview/CloseBtn.tsx
+++ src/Preview/CloseBtn.tsx
@@ -1,16 +1,23 @@
+import classnames from 'classnames';
 import * as React from 'react';
 
 export interface CloseBtnProps {
   prefixCls: string;
   icon?: React.ReactNode;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
+  className?: string;
+  style?: React.CSSProperties;
 }
 
 export default function CloseBtn(props: CloseBtnProps) {
-  const { prefixCls, icon, onClick } = props;
+  const { prefixCls, icon, onClick, className, style } = props;
 
   return (
-    <button className={`${prefixCls}-close`} onClick={onClick}>
+    <button
+      className={classnames(`${prefixCls}-close`, className)}
+      style={style}
+      onClick={onClick}
+    >
       {icon}
     </button>
   );
